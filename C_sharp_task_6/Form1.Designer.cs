2719278d048afbd88a9e40e287a6579a74bf2459
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-05-27 23:13:38 +0300

    Initial commit

---
+++ C_sharp_task_6/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace C_sharp_task_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(107, 175);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(382, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(107, 115);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(382, 24);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выбрать библиотеку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(22, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выбранная библиотека";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(198, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(887, 23);
+            this.label2.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(507, 115);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(509, 220);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(22, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Класс";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(22, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Метод";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(467, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Выполнить метод";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 303);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 303);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(264, 303);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(389, 303);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 718);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBox4;
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+
+        #endregion
+    }
+}
---
+++ C_sharp_task_6/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using TaskLibrary;
+
+
+namespace C_sharp_task_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog dialog = new OpenFileDialog())
+                {
+                    dialog.Title = "Выберите .dll файл";
+                    dialog.Filter = "DLL Files (*.dll)|*.dll|All Files (*.*)|*.*";
+
+                    DialogResult result = dialog.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.FileName))
+                    {
+                        string selectedFilePath = dialog.FileName;
+                        label2.Text = selectedFilePath;
+                        Assembly assembly = Assembly.LoadFrom(selectedFilePath);
+                        Type interfaceType = assembly.GetType("ClassLibrary.Paper");
+
+                        Type[] types = assembly.GetTypes();
+
+
+                        foreach (Type type in types)
+                        {
+                            if (interfaceType.IsAssignableFrom(type) && type.IsClass && !type.IsAbstract)
+                            {
+                                comboBox2.Items.Add(type.FullName);
+                                richTextBox1.Text += type.ToString() + "\n";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                Console.WriteLine("Ошибка загрузки типов:");
+                foreach (Exception loaderException in ex.LoaderExceptions)
+                {
+                    richTextBox1.Text += loaderException.Message;
+                }
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            string className = comboBox.SelectedItem.ToString();
+            Assembly assembly = Assembly.LoadFile(label2.Text);
+            Type classType = assembly.GetType(className);
+            MethodInfo[] methods = classType.GetMethods();
+
+            comboBox1.Items.Clear();
+            foreach (MethodInfo method in methods)
+            {
+                comboBox1.Items.Add(method.Name);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string var1 = textBox1.Text;
+            string var2 = textBox2.Text;
+            string var3 = textBox3.Text;
+            string var4 = textBox4.Text;
+
+            string className = comboBox2.SelectedItem.ToString();
+            Assembly assembly = Assembly.LoadFile(label2.Text);
+            Type classType = assembly.GetType(className);
+            ConstructorInfo constructor = classType.GetConstructors()[0];
+            object[] arguments = { "Johny", "Johny", "Johny", null, null };
+            object instance = constructor.Invoke(arguments);
+            string methodName = comboBox1.SelectedItem.ToString();
+            MethodInfo method = classType.GetMethod(methodName);
+
+            if (var1 != "" && var2 != "" && var3 != "" && var4 != "")
+            {
+                richTextBox1.Text = method.Invoke(instance, new[] { var1, var2, var3, var4 }).ToString();
+            }
+            else if (var1 != "" && var2 != "" && var3 != "")
+            {
+                richTextBox1.Text = method.Invoke(instance, new[] { var1, var2, var3 }).ToString();
+            }
+            else if (var1 != "" && var2 != "")
+            {
+                richTextBox1.Text = method.Invoke(instance, new[] { var1, var2 }).ToString();
+            }
+            else if (var1 != "")
+            {
+                richTextBox1.Text = method.Invoke(instance, new[] { var1 }).ToString();
+            }
+            else
+            {
+                richTextBox1.Text = method.Invoke(instance, null).ToString();
+            }
+        }
+    }
+}
